6fc8303835891644ef9363fe9f019821efb1854b
Douglas Rocha <douglas_igt14@hotmail.com>
2023-12-01 19:14:30 -0300

    WIP

---
+++ resources/views/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kivibe</title>
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+</head>
+<body>
+    <div class="conteiner">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <img src="{{ asset('img/logo3.png') }}" class="img-fluid img-logo" alt="...">
+            </div>
+        </div>
+        <div class="content-tabs">
+            <div><button type="button" class="btn btn-tab">Sing In</button></div>
+            <div><button type="button" class="btn btn-tab">Sing up</button></div>    
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
