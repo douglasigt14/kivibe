28cff126c2963d9536d6b2cdf52c33c6a7a6a99e
Douglas Rocha <douglas_igt14@hotmail.com>
2023-12-01 22:32:19 -0300

    WIP

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +9,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-</head>
 <body>
     <div class="conteiner">
         <div class="row justify-content-center">
@@ -18,29 +17,28 @@
             </div>
         </div>
         <div class="content-tabs">
-            <div class="content-tab"><button type="button" class="btn  btn-tab">Sing In</button></div>
-            <div class="content-tab"><button type="button" class="btn btn-tab">Sing In</button></div>    
+            <div class="content-tab"><button type="button" class="btn btn-tab">Entrar</button></div>
+            <div class="content-tab"><button type="button" class="btn btn-tab">Cadastrar</button></div>    
         </div>
         <div class="sing-in-content">
             <div>
                 <button type="button" class="btn btn-dark btn-circle btn-social">
                     <i class="fa fa-google fa-2x"></i>
-                  </button>
+                </button>
             </div>
             <div>
                 <button type="button" class="btn btn-dark btn-circle btn-social">
                     <i class="fa fa-apple fa-2x"></i>
-                  </button>
+                </button>
             </div>
-           
         </div>
         <div class="form-floating mb-3 mt-5">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
+            <input type="email" class="form-control" id="floatingInput" placeholder="nome@exemplo.com">
+            <label for="floatingInput">Endereço de E-mail</label>
         </div>
         <div class="form-floating mb-3 mt-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Password</label>
+            <input type="password" class="form-control" id="floatingInput" placeholder="Digite sua senha">
+            <label for="floatingInput">Senha</label>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
